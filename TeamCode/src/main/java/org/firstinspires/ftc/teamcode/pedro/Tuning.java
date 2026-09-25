a18a09ffcc4cd4242e83a74630756afb0a98ad8b
naushadwaqar <nwaqar970209@gmail.com>
2026-09-25 17:11:56 -0400

    nothing but ill just commit it b4 i forget

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
@@ -1,5 +1,15 @@
 package org.firstinspires.ftc.teamcode.pedro;
+import org.firstinspires.ftc.teamcode.pedro.procedures.MecanumTuner;
+import com.pedropathing.tuning.autotune.Procedure;
+import com.pedropathing.tuning.autotune.Tuner;
+
 
 public class Tuning {
     // Tuners go here
+
+    @Tuner
+    public static Procedure mecanumTuner() {
+        return new MecanumTuner();
+    }
 }
+
